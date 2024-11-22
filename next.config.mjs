97bc4d1436db108f75ce55bfe317fbbe7694525d
Elostay <nastia60203@gmail.com>
2024-11-22 14:53:23 +0200

    bugfix

--- next.config.mjs
+++ next.config.mjs
@@ -15,25 +15,9 @@ const nextConfig = {
       },
     ],
   },
-  reactStrictMode: false,
   images: {
     domains: ['ftp.goit.study'],
   },
 };
 
 export default nextConfig;
-/*
-
-sm	640px	@media (min-width: 640px) { ... }
-
-md	768px	@media (min-width: 768px) { ... }
-
-lg	1024px	@media (min-width: 1024px) { ... }
-
-xl	1280px	@media (min-width: 1280px) { ... }
-
-2xl 1536px	@media (min-width: 1536px) { ... }
-
-
-
-*/
